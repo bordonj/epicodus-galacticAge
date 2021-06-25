ba35ceede20d880ab0bda02d3768605eb59d40fc
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-25 16:06:01 -0700

    add tests passing different arguments to get full coverage of lines

--- __tests__/lifeExpect.test.js
+++ __tests__/lifeExpect.test.js
@@ -1,19 +1,31 @@
 import LifeExpect from "../src/js/lifeExpect";
 
 describe('Life Expect', () => {
-  let lifeExpect;
-
-  beforeEach(() => {
-    lifeExpect = new LifeExpect('female', 30, 16.8, 2, false, false, 1, 6, false);
-  })
-
+  
   test('should create a lifeExpect object that can calculate life expectancy based on some demographics', () => {
+    let lifeExpect = new LifeExpect('female', 30, 16.8, 2, false, false, 1, 6, false);
     console.log('lifeExpect', lifeExpect);
     expect(lifeExpect.lifeExpectYears).toEqual(90.6);
   })
   test('lifeExpect object should contain variable that calulates how many years user has left to live', () => {
-    console.log('yearsLeft', yearsLeft);
+    let lifeExpect = new LifeExpect('female', 30, 16.8, 2, false, false, 1, 6, false);
+    console.log('yearsLeft', lifeExpect.yearsLeft);
     expect(lifeExpect.yearsLeft).toEqual(60.6);
+  })
+  test(`lifeExpect object should show different lifeExpectYears for different argument inputs ('female', 60, 45, 0, true, true, 1, 1, false)`, () => {
+    let lifeExpect = new LifeExpect('female', 60, 45, 0, true, true, 1, 1, false);
+    console.log('yearsLeft', lifeExpect.yearsLeft);
+    expect(lifeExpect.lifeExpectYears).toBeLessThan(70);
+  })
+  test(`lifeExpect object should show different lifeExpectYears for argument inputs ('male', 60, 45, 1, true, true, 0, 1, true)`, () => {
+    let lifeExpect = new LifeExpect('male', 33, 45, 1, true, true, 0, 1, true);
+    console.log('yearsLeft', lifeExpect.yearsLeft);
+    expect(lifeExpect.lifeExpectYears).toBeLessThan(60);
+  })
+  test(`lifeExpect object should show different lifeExpectYears for different argument inputs ('non-conforming', 60, 45, 0, true, true, 1, 1, false`, () => {
+    let lifeExpect = new LifeExpect('non-conforming', 30, 45, 1, true, true, 0, 1, true);
+    console.log('yearsLeft', lifeExpect.yearsLeft);
+    expect(lifeExpect.lifeExpectYears).toBeLessThan(50);
   })
   
 })

f281b18a3e410534c7b05f1e1718ac19ba05e438
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-25 10:40:12 -0700

    add venus class and test to check venusAge

---
+++ __tests__/venus.test.js
@@ -0,0 +1,14 @@
+import Venus from "../src/js/venus";
+
+describe('Venus', () => {
+  let venus;
+
+  beforeEach(() => {
+    venus = new Venus(30);
+  })
+
+  test('should correctly create a venus object and its converted age', () => {
+    console.log('venus class', venus);
+    expect(venus.venusAge).toEqual(18.6);
+  })
+})
---
+++ src/js/venus.js
@@ -0,0 +1,6 @@
+// - Venus year is .62 Earth years
+export default class Venus {
+  constructor(age) {
+
+  }
+}

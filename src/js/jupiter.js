1c0039ff7390c645ccb81c6fb73c0e28238c31ed
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-25 16:44:48 -0700

    add jupiterLifeExpect() to calculate years left on jupiter

--- src/js/jupiter.js
+++ src/js/jupiter.js
@@ -1,7 +1,10 @@
-// - Jupiter year is 11.86 Earth years
+import LifeExpect from "./lifeExpect";
 export default class Jupiter {
   constructor(age) {
     this.jupiterAge = parseFloat((age * 11.86).toFixed(1));
     this.earthAge = age;
+  }
+  jupiterLifeExpect(yearsLeft) {
+    this.jupiterYearsLeft = parseFloat((yearsLeft * 11.86).toFixed(1)) 
   }
 }

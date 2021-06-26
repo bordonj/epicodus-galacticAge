6f430a99c60693b4eef1c1c27b0492f1543302f8
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-25 17:17:08 -0700

    add venusLifeExpect() to calculate years left on venus, pass test

--- src/js/venus.js
+++ src/js/venus.js
@@ -4,7 +4,8 @@ export default class Venus {
     this.venusAge = parseFloat((age * .62).toFixed(1));
     this.earthAge = age;
   }
-  venusLifeExpect() {
-    
+  venusLifeExpect(yearsLeft) {
+    this.venusYearsLeft = parseFloat((yearsLeft * .62).toFixed(1));
+
   }
 }

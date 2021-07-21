0a6303cd80a9f3803e10b7d5884d3a552dacb4b0
bordonj <80655182+bordonj@users.noreply.github.com>
2021-07-21 22:02:30 +0000

    Deploying to gh-pages from @ bordonj/epicodus-galacticAge@fc641054565881fc397df4f30ec41a766842aac7 🚀

--- src/js/jupiter.js
+++
@@ -1,9 +0,0 @@
-export default class Jupiter {
-  constructor(age) {
-    this.jupiterAge = parseFloat(((age / 11.86)).toFixed(1));
-    this.earthAge = age;
-  }
-  jupiterLifeExpect(yearsLeft) {
-    this.jupiterYearsLeft = parseFloat((yearsLeft / 11.86).toFixed(1));
-  }
-}
--- src/js/mars.js
+++
@@ -1,9 +0,0 @@
-export default class Mars {
-  constructor(age) {
-    this.marsAge = parseFloat((age / 1.88).toFixed(1));
-    this.earthAge = age;
-  }
-  marsLifeExpect(yearsLeft) {
-    this.marsYearsLeft = parseFloat((yearsLeft / 1.88).toFixed(1));
-  }
-}
